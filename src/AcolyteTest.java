21550aff0829e00cc4e1e2627736c614ae0c4834
ACOLYTE <maksikxx@ukr.net>
2022-08-13 16:34:35 +0300

    commit db 3

---
+++ src/AcolyteTest.java
@@ -0,0 +1,103 @@
+import java.sql.*;
+import java.util.concurrent.Callable;
+
+public class AcolyteTest{
+
+    public static Statement stat;
+    public static void main(String[] args) throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:My_cats.db");
+            System.out.println("Connected");
+            stat = connection.createStatement();
+            stat.execute("CREATE TABLE if not exists 'types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT , 'type' text NOT NULL);");
+
+
+
+
+
+
+
+
+
+
+            } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+            addAll(types);
+
+
+
+    }
+    static String[] types = new String[]{"Абиссинская кошка",
+            "Австралийский мист",
+            "Американская жесткошерстная",
+            "Американская короткошерстная",
+            "Американский бобтейл",
+            "Американский кёрл",
+            "Балинезийская кошка",
+            "Бенгальская кошка",
+            "Бирманская кошка",
+            "Бомбейская кошка",
+            "Бразильская короткошёрстная",
+            "Британская длинношерстная",
+            "Британская короткошерстная",
+            "Бурманская кошка",
+            "Бурмилла кошка",
+            "Гавана",
+            "Гималайская кошка",
+            "Девон-рекс",
+            "Донской сфинкс",
+            "Европейская короткошерстная",
+            "Египетская мау",
+            "Канадский сфинкс",
+            "Кимрик",
+            "Корат",
+            "Корниш-рекс",
+            "Курильский бобтейл",
+            "Лаперм",
+            "Манчкин",
+            "Мейн-ку́н",
+            "Меконгский бобтейл",
+            "Мэнкс кошка",
+            "Наполеон",
+            "Немецкий рекс",
+            "Нибелунг",
+            "Норвежская лесная кошка",
+            "Ориентальная кошка",
+            "Оцикет",
+            "Персидская кошка",
+            "Петерболд",
+            "Пиксибоб",
+            "Рагамаффин",
+            "Русская голубая кошка",
+            "Рэгдолл",
+            "Саванна",
+            "Селкирк-рекс",
+            "Сиамская кошка",
+            "Сибирская кошка",
+            "Сингапурская кошка",
+            "Скоттиш-фолд",
+            "Сноу-шу",
+            "Сомалийская кошка",
+            "Тайская кошка",
+            "Тойгер",
+            "Тонкинская кошка",
+            "Турецкая ангорская кошка",
+            "Турецкий ван",
+            "Украинский левкой",
+            "Чаузи",
+            "Шартрез",
+            "Экзотическая короткошерстная",
+            "Японский бобтейл"
+    };
+        public static void addType(String type) throws SQLException {
+          stat.execute("INSERT INTO 'types' ('type') VALUES ('"+type+"');");
+        }
+        public static void addAll(String[] all ) throws SQLException {
+            for(int i=0;i<types.length;i++){
+            stat.execute("INSERT INTO 'types'('type') VALUES ('"+types[i]+"');");}
+        }
+}
